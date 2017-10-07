bb09c12cd0b5f5a9406fb1211ed2dec5283cb35a
msschenn <30837349+msschenn@users.noreply.github.com>
2017-10-07 16:33:19 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -99,7 +99,7 @@
 
     //owl carousel
     $('.owl-carousel').owlCarousel({
-      autoPlay: 3000, //Set AutoPlay to 3 seconds
+      autoPlay: 60000, //Set AutoPlay to 3 seconds
  
       items : 1,
       itemsDesktop : [1199,1],
